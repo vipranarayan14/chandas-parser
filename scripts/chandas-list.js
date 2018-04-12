b2f174b14d321055fa7397d3a246835687835f11
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-12 10:16:59 +0530

    add 'uShNik' class of metres.

--- scripts/chandas-list.js
+++ scripts/chandas-list.js
@@ -3,42 +3,60 @@ const metres = [
   {
     caesura: [],
     definition: 'guH',
-    examples: ['shrIshaH | pAyAt ||'],
+    examples: [
+      'shrIshaH |' +
+      'pAyAt ||'
+    ],
     name: 'shrIH',
     type: 'uktA'
   },
   {
     caesura: [],
     definition: 'gau strI',
-    examples: ['shrImAn viShnuH | nityaM dhyeyaH ||'],
+    examples: [
+      'shrImAn viShnuH |' +
+      'nityaM dhyeyaH ||'
+    ],
     name: 'strI',
     type: 'atyuktA'
   },
   {
     caesura: [],
     definition: 'mO nArI',
-    examples: ['nArINAM kalyANI | mAM pAyAtsA vANI ||'],
+    examples: [
+      'nArINAM kalyANI |' +
+      'mAM pAyAtsA vANI ||'
+    ],
     name: 'nArI',
     type: 'madhyA'
   },
   {
     caesura: [],
     definition: 'rO mRRigIH',
-    examples: ['sA mRRigIlOchanI | rAdhikA shrIpatEH ||'],
+    examples: [
+      'sA mRRigIlOchanI |' +
+      'rAdhikA shrIpatEH ||'
+    ],
     name: 'mRRigI',
     type: 'madhyA'
   },
   {
     caesura: [],
     definition: 'mgau cetkanyA',
-    examples: ['bhAsvatkanyA saikA dhanyA | yasyAH kUlE kRRiShNO.akhElat ||'],
+    examples: [
+      'bhAsvatkanyA saikA dhanyA |' +
+      'yasyAH kUlE kRRiShNO.akhElat ||'
+    ],
     name: 'kanyA',
     type: 'pratiShTA'
   },
   {
     caesura: [],
     definition: 'bhgau giti pa~NktiH',
-    examples: ['bhAsvatkanyA saikA dhanyA | yasyAH kUlE kRRiShNO.akhElat ||'],
+    examples: [
+      'bhAsvatkanyA saikA dhanyA |' +
+      'yasyAH kUlE kRRiShNO.akhElat ||'
+    ],
     name: 'pa~NktiH',
     type: 'supratiShTA'
   },
@@ -46,8 +64,10 @@ const metres = [
     caesura: [],
     definition: 'tyau stastanumadhyA',
     examples: [
-      'mUrtirmurashatrOratyadbhutarUpA | AstAM mama cittE nityaM tanumadhyA ||',
-      'nashyanti dadarsha vRRindAni kapIndraH | hArINyabalAnAM hArINyabalAnAm ||'
+      'mUrtirmurashatrOratyadbhutarUpA |' +
+      'AstAM mama cittE nityaM tanumadhyA ||',
+      'nashyanti dadarsha vRRindAni kapIndraH |' +
+      'hArINyabalAnAM hArINyabalAnAm ||'
     ],
     name: 'tanumadhyA',
     type: 'gAyatrI'
@@ -55,28 +75,70 @@ const metres = [
   {
     caesura: [],
     definition: 'shashivadanA nyau',
-    examples: ['shashivadanAnAM vrajataruNInAm | dadhighaTabhedaM madhuripuraichchhat ||'],
+    examples: [
+      'shashivadanAnAM vrajataruNInAm |' +
+      'dadhighaTabhedaM madhuripuraichchhat ||'
+    ],
     name: 'shashivadanA',
     type: 'gAyatrI'
   },
   {
     caesura: [],
     definition: 'vidyullEkhA mO maH',
-    examples: ['varShAkAlE kAlE mEghAchchhannAkAshE | vidyullEkhA bhAntyAH sarvairAlOkhyantE ||'],
+    examples: [
+      'varShAkAlE kAlE mEghAchchhannAkAshE |' +
+      'vidyullEkhA bhAntyAH sarvairAlOkhyantE ||'
+    ],
     name: 'vidyullEkhA',
     type: 'gAyatrI'
   },
   {
     caesura: [],
     definition: 'tsau chedvasumatI',
-    examples: ['sAstE vasumatI yAstE vasumatI | puNyAkaravatI puNyAkarabhavA ||'],
+    examples: [
+      'sAstE vasumatI yAstE vasumatI |' +
+      'puNyAkaravatI puNyAkarabhavA ||'
+    ],
     name: 'vasumatI',
     type: 'gAyatrI'
   },
   {
     caesura: [],
+    definition: 'msau gaH syAnmadalEkhA',
+    examples: [
+      'ra~NgE bAhuvirugNAt dantIndrAnmadalEkhA |' +
+      'lagnAbhUtmurashatrau kastUrIrasacharchA ||'
+    ],
+    name: 'madalEkhA',
+    type: 'uShNik'
+  },
+  {
+    caesura: [],
+    definition: 'kumAralalitA jsau g',
+    examples: [
+      'murAritanuvallI kumAralalitA sA |' +
+      'vrajaiNanayanAnAM tatAna mudamuchchaiH ||'
+    ],
+    name: 'kumAralalitA',
+    type: 'uShNik'
+  },
+  {
+    caesura: [],
+    definition: 'saragai haMsamAlA',
+    examples: [
+      'dhavalA haMsamAlA sukhapUrvaM vishAlA |' +
+      'vimalAmbhastaDAkE bhayahInA nidadrau ||'
+    ],
+    name: 'haMsamAlA',
+    type: 'uShNik'
+  },
+  {
+    caesura: [],
     definition: 'guH',
-    examples: ['shrIshaH | pAyAt ||'],
+    examples: [
+      'shrIshaH |' +
+      'pAyAt ||'
+    ],
     name: 'shrIH',
     type: 'uktA'
   },
